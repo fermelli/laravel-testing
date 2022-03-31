83694cc43a4f5b5f165689ec6cb65ff7b1790d40
fermelli <luisfernandosalgadomiguez@gmail.com>
2022-03-31 16:37:18 -0400

    Calculate Fibonacci

---
+++ app/Src/Fibonacci.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Src;
+
+class Fibonacci
+{
+    public function calculate($index)
+    {
+
+        if ($index == 0) {
+            return 0;
+        }
+
+        if ($index == 1) {
+            return 1;
+        }
+
+        return $this->calculate($index - 1) + $this->calculate($index - 2);
+    }
+}
